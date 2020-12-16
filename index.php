0fac54d10b4a11a7b547ff52aaa8b558845eba48
miglezub <miglezub@gmail.com>
2020-12-16 09:44:35 +0200

    delete client credentials and hide API key

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include("include/login_handler.php"); ?>
 <html>
 	<head>
-		<script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCE6LDTVHDg17JwOoOnTVcvkA4Ef6EDSbw&callback=initMap"></script>
+		<script defer src="https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap"></script>
 		<script src="./js/script.js"></script>
 		<script src="./calendar/js/daypilot/daypilot-all.min.js"></script>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
